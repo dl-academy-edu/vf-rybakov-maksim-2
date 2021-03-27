b3fd1a7805d647f27b447d2f4a66175c602ab701
rybakma1 <rybakov-mv@yandex.ru>
2021-03-27 14:52:32 +0400

    Makes corrections for validation
    - Добавил пустую последнюю строку в файлах.
    - Заменил тег <p> на <time> у карточек на странице blog.
    - Добавил ссылки к иконкам социальных сетей в футере.

--- src/pages/catalog/index.js
+++ src/pages/catalog/index.js
@@ -2,8 +2,8 @@ var buttonMobileHeader = document.querySelector('.mobile-menu_open-js');
 var buttonCloseMobileHeader = document.querySelector('.mobile-menu_close-js');
 var mobileHeader = document.querySelector('.mobile-menu_js');
 var mobileMenu = document.querySelector('.mobile-menu');
- 
- 
+
+
 buttonMobileHeader.addEventListener('click', function () {
     mobileHeader.classList.add('mobile-menu__open')
     buttonMobileHeader.classList.add('display-none')
@@ -11,7 +11,7 @@ buttonMobileHeader.addEventListener('click', function () {
     mobileMenu.classList.add('mobile-menu__background');
     mobileMenu.classList.remove('display-none');
 })
- 
+
 buttonCloseMobileHeader.addEventListener('click', function() {
     mobileHeader.classList.remove('mobile-menu__open')
     buttonCloseMobileHeader.classList.add('display-none')
